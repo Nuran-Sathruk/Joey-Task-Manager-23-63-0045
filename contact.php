7452407b5f38236cfc37ef9ea82206cf234cc754
Nuran-Sathruk <158542308+Nuran-Sathruk@users.noreply.github.com>
2024-05-26 20:39:00 +0300

    Delete contact.php

--- contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $response = "Thank you, $name! We have received your message.";
-    echo $response;
-}
-?>
